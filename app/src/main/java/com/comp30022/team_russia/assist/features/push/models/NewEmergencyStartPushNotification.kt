ec08c731f5ad3a4160540f0b2482c3bcd257130a
Radium <ztl8702@users.noreply.github.com>
2018-10-17 23:16:11 +1100

    fix emergency call (#208)

--- app/src/main/java/com/comp30022/team_russia/assist/features/push/models/NewEmergencyStartPushNotification.kt
+++ app/src/main/java/com/comp30022/team_russia/assist/features/push/models/NewEmergencyStartPushNotification.kt
@@ -5,5 +5,5 @@ data class NewEmergencyStartPushNotification (
         @SerializedName("eventID") var eventId: Int = -1,
         @SerializedName("senderID") var senderId: Int = -1,
         @SerializedName("senderName") var senderName: String,
-        @SerializedName("mobileNumber") var mobileNumber: Int = -1
+        @SerializedName("mobileNumber") var mobileNumber: String = ""
 )
